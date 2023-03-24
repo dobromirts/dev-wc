00cfb45142017cef10297c8973b6cd908cbe914e
dobromirts <46093564+dobromirts@users.noreply.github.com>
2023-03-24 11:06:50 +0200

    untitled-app (44) initial version

---
+++ src/app/master-view/master-view.ts
@@ -0,0 +1,33 @@
+import { html, css, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { defineComponents, IgcCalendarComponent } from 'igniteui-webcomponents';
+
+defineComponents(IgcCalendarComponent);
+
+@customElement('app-master-view')
+export default class MasterView extends LitElement {
+  static styles = css`
+    :host {
+      height: 100%;
+      display: flex;
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: flex-start;
+    }
+    .calendar {
+      width: max-content;
+      height: max-content;
+      flex-grow: 1;
+      flex-basis: 0;
+    }
+  `;
+
+  render() {
+    return html`
+      <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>
+      <link href='https://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet'>
+      <link rel='stylesheet' href='../../ig-theme.css'>
+      <igc-calendar ?hide-header="${false}" header-orientation="horizontal" class="calendar"></igc-calendar>
+    `;
+  }
+}
